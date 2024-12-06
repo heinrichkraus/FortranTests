457f746bb7809bd26add9130e789d27afc124bbc
Heinrich Kraus <heinrich.kraus@itwm.fraunhofer.de>
2024-12-06 14:48:49 +0100

    Adjust compilation scripts

--- macros/compile_gfortran.sh
+++ macros/compile_gfortran.sh
@@ -1,1 +1,4 @@
-gfortran -cpp -ffree-line-length-none -flto -O3 -finline-arg-packing -finline-matmul-limit=50 matrix3.f90 matrix.f90 main.f90 -o gfort.o
+gfortran -cpp -c -ffree-line-length-none -flto -O3 -finline-arg-packing -finline-matmul-limit=50 matrix3.f90 matrix.f90 main.f90
+gfortran -cpp -ffree-line-length-none -flto -O3 -finline-arg-packing -finline-matmul-limit=50 matrix3.o matrix.o main.o -o gfort.o
+
+rm matrix3.o matrix.o main.o
--- macros/compile_ifort.sh
+++ macros/compile_ifort.sh
@@ -1,3 +1,6 @@
 module load compiler/intel/oneapi-latest
 
-ifort -fpp -finline -O3 -xHost -ipo -fp-model strict matrix3.f90 matrix.f90 main.f90 -o ifort.o
+ifort -fpp -c -finline -O3 -xHost -ipo -fp-model strict matrix3.f90 matrix.f90 main.f90
+ifort -fpp -finline -O3 -xHost -ipo -fp-model strict matrix3.o matrix.o main.o -o ifort.o
+
+rm matrix3.o matrix.o main.o
