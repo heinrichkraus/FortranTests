a57fe9114ab0502cccfdcdc5ff963bcb884a7791
Heinrich Kraus <heinrich.kraus@itwm.fraunhofer.de>
2024-12-09 09:29:03 +0100

    Compilation scripts

--- macros/compile_gfortran.sh
+++ macros/compile_gfortran.sh
@@ -1,4 +1,5 @@
-gfortran -cpp -c -ffree-line-length-none -flto -O3 -finline-arg-packing -finline-matmul-limit=50 matrix3.f90 matrix.f90 main.f90
-gfortran -cpp -ffree-line-length-none -flto -O3 -finline-arg-packing -finline-matmul-limit=50 matrix3.o matrix.o main.o -o gfort.o
+#-flto (link time optimization) can produce better results
+gfortran -cpp -flto -ffree-line-length-none -O3 -finline-arg-packing -finline-matmul-limit=50 matrix3.f90 matrix.f90 main.f90 -c
+gfortran -cpp -flto -ffree-line-length-none -O3 -finline-arg-packing -finline-matmul-limit=50 matrix3.o   matrix.o   main.o   -o gfort.o
 
 rm matrix3.o matrix.o main.o
--- macros/compile_ifort.sh
+++ macros/compile_ifort.sh
@@ -1,6 +1,6 @@
 module load compiler/intel/oneapi-latest
-
-ifort -fpp -c -finline -O3 -xHost -ipo -fp-model strict matrix3.f90 matrix.f90 main.f90
-ifort -fpp -finline -O3 -xHost -ipo -fp-model strict matrix3.o matrix.o main.o -o ifort.o
+# -ipo produces faster code and allows inlinng
+ifort -ipo -fpp -O3 matrix3.f90 matrix.f90 main.f90 -c
+ifort -ipo -fpp -O3 matrix3.o   matrix.o   main.o   -o ifort.o
 
 rm matrix3.o matrix.o main.o
