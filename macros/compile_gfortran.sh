1bf66b4e51b43845152d54ca8a765866adfbf293
Heinrich Kraus <heinrich.kraus@itwm.fraunhofer.de>
2025-02-07 09:59:08 +0100

    Fix compilation

--- macros/compile_gfortran.sh
+++ macros/compile_gfortran.sh
@@ -1,5 +1,5 @@
 #-flto (link time optimization) can produce better results
-gfortran -cpp -flto -ffree-line-length-none -O3 -finline-arg-packing -finline-matmul-limit=50 matrix3.f90 matrix.f90 main.f90 -c
-gfortran -cpp -flto -ffree-line-length-none -O3 -finline-arg-packing -finline-matmul-limit=50 matrix3.o   matrix.o   main.o   -o gfort.o
+gfortran -cpp -ffree-line-length-none -O3 -finline-arg-packing -finline-matmul-limit=50 matrix3.f90 matrix.f90 main.f90 -c
+gfortran -cpp -ffree-line-length-none -O3 -finline-arg-packing -finline-matmul-limit=50 matrix3.o   matrix.o   main.o   -o gfort.o
 
 rm matrix3.o matrix.o main.o
--- macros/compile_ifort.sh
+++ macros/compile_ifort.sh
@@ -1,6 +1,6 @@
 module load compiler/intel/oneapi-latest
 # -ipo produces faster code and allows inlinng
-ifort -ipo -fpp -O3 matrix3.f90 matrix.f90 main.f90 -c
-ifort -ipo -fpp -O3 matrix3.o   matrix.o   main.o   -o ifort.o
+ifort -fpp -O3 matrix3.f90 matrix.f90 main.f90 -c
+ifort -fpp -O3 matrix3.o   matrix.o   main.o   -o ifort.o
 
 rm matrix3.o matrix.o main.o
